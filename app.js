bfc1d1e67c2704af37bd7b64240270f2e71d4934
Daniil Kopylov <Shinoinochi@yandex.ru>
2023-07-23 19:21:21 +0500

    Обработка валидации

--- app.js
+++ app.js
@@ -28,25 +28,25 @@ app.use(express.json());
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom(validUrl),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom(validUrl),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 }), createUser);
 
+app.use(auth);
+app.use(errors());
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use(auth);
-app.use(errors());
 app.patch('*', (req, res) => {
   res.status(404).send({ message: 'Здесь ничего нет' });
 });
